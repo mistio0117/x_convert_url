f25e2ca3e4fbb631b1149efcdc5998a25d2b90f2
mistio0117 <afsmtb57@gmail.com>
2024-01-04 03:40:21 +0900

    test9

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ async def on_message(message):
     # emoji ="👍"
     # await message.add_reaction(emoji)
     # await message.channel.send("おはよう！！")
+    if message.author.bot:
+        return
+    
     if message.content.find('https:\/\/x.com'):
         vx_url = message.content
         vx_url = vx_url.replace('x.com', 'vxtwitter.com')
-        await message.channel.send(vx_url)
+        await message.channel.send("in!!")
+    else:
+        await message.channel.send("not in!!")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 # Web サーバの立ち上げ
